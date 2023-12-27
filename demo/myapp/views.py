f17d862f7606c5ff7bc25273f96e76446108277e
Smriti Jojo <smriti30jojo@gmail.com>
2023-12-27 20:44:35 +0530

    project added

---
+++ demo/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,HttpResponse
+from .models import TodoItem
+# Create your views here.
+def home(request):
+    return HttpResponse("Welcome Smriti")
+
+def todos(request):
+    items= TodoItem.objects.all()
+    return render(request ,"todos.html",{"todos":items})
